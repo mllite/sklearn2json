ee7c2f04a0c81f507bc0da456e591b6ad9a403c9
Antoine CARME <antoine.carme@outlook.com>
2024-01-17 15:00:22 +0100

    Added Random Forests

--- __init__.py
+++ __init__.py
@@ -5,12 +5,14 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import RidgeClassifier
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 import sklearn2json.converters.decison_tree_converter as tree_cvt
 import sklearn2json.converters.ridge_converter as ridge_cvt
 import sklearn2json.converters.svm_converter as svm_cvt
 import sklearn2json.converters.mlp_converter as mlp_cvt
 import sklearn2json.converters.xgb_converter as xgb_cvt
+import sklearn2json.converters.random_forest_converter as rf_cvt
 
 def convert_model(clf):
     print("CONVERT_MODEL ", clf.__class__)    
@@ -43,6 +45,12 @@ def convert_model(clf):
         return lConverter.convert_classifier(clf)
     if(clf.__class__ == xgb.XGBRegressor):
         lConverter = xgb_cvt.xgb_converter()
+        return lConverter.convert_regressor(clf)
+    if(clf.__class__ == sklearn.ensemble.RandomForestClassifier):
+        lConverter = rf_cvt.random_forest_converter()
+        return lConverter.convert_classifier(clf)
+    if(clf.__class__ == sklearn.ensemble.RandomForestRegressor):
+        lConverter = rf_cvt.random_forest_converter()
         return lConverter.convert_regressor(clf)
     
     print("WARNING_CANNOT_CONVERT_MODEL ", clf.__class__)    
